7d4d1e1d049d3936a7ced290a52e98adb730f2a1
Patrick Pichon <patrick@pichon.me>
2023-03-02 22:12:01 +0100

    look after the Model Manuf Mapping

--- z4d_certified_devices/__init__.py
+++ z4d_certified_devices/__init__.py
@@ -10,7 +10,6 @@ from .version import __version__
 def z4d_import_device_configuration(self, path_name):
 
     # Read DeviceConf for backward compatibility 
-    self.DeviceConf = {}
     model_certified = path_name + "Certified"
     plugin_version = self.pluginParameters["PluginVersion"]
 
@@ -35,11 +34,9 @@ def z4d_import_device_configuration(self, path_name):
                         model_definition = json.load(handle)
                     except ValueError as e:
                         Domoticz.Error("--> JSON ConfFile: %s load failed with error: %s" % (filename, str(e)))
-
                         continue
                     except Exception as e:
                         Domoticz.Error("--> JSON ConfFile: %s load general error: %s" % (filename, str(e)))
-
                         continue
 
                 try:
@@ -50,13 +47,20 @@ def z4d_import_device_configuration(self, path_name):
 
                     self.log.logging( "z4dCertifiedDevices", "Debug", "--> Config for %s/%s" % (str(brand), str(device_model_name)))
                     if "MinPluginVersion" in model_definition and plugin_version < model_definition["MinPluginVersion"]:
-                        self.log.logging( "z4dCertifiedDevices", "Log", "Skip this Certified device %s-%s requires Plugin version %s" % (
+                        self.log.logging( "z4dCertifiedDevices", "Log", "Certified Devices load skip this Certified device %s-%s requires Plugin version %s" % (
                             str(brand), str(device_model_name), model_definition["MinPluginVersion"] ),)
                         continue
 
                     self.DeviceConf[device_model_name] = dict(model_definition)
+                    if "Identifier" in model_definition:
+                        self.log.logging( "z4dCertifiedDevices", "Debug", "--> Identifier found %s" % (str(model_definition["Identifier"])) )
+                        for x in model_definition["Identifier"]:
+                            self.log.logging( "z4dCertifiedDevices", "Debug", "-->     %s" %x)
+                            self.ModelManufMapping[ (x[0], x[1] )] = device_model_name
+
                 except Exception:
                     Domoticz.Error("--> Unexpected error when loading a configuration file")
 
     self.log.logging("z4dCertifiedDevices", "Debug", "--> Config loaded: %s" % self.DeviceConf.keys())
+    self.log.logging("z4dCertifiedDevices", "Status", "Certified Devices ModelManufMapping loaded - %s" %self.ModelManufMapping.keys())
     self.log.logging("z4dCertifiedDevices", "Status", "Certified Devices loaded - %s confs loaded" %len(self.DeviceConf))
